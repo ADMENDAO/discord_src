6e446403a61f0f1dc53db44da05f98b140b03e80
anone <anone99@gmail.com>
2022-02-07 12:13:33 +0900

    コマンドでスプレッドシートに反映

--- src/plugins/logger.js
+++ src/plugins/logger.js
@@ -1,14 +1,3 @@
-const colors = require('colors/safe');
-const logger = require('tracer').colorConsole({
-  filters: [
-     colors.white,
-    {
-      trace: colors.white,
-      debug: colors.blue,
-      info: colors.green,
-      warn: colors.yellow,
-      error: [colors.red.bold]
-     }
-	]
-})
+//const colors = require('colors/safe');
+const logger = require('tracer').console()
 module.exports = logger;
